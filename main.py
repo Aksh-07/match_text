92968d12a652c665149d2111c82e22fe23b77dae
Aksh <akshdeep434@gmail.com>
2022-04-19 15:59:22 +0530

    2nd commit

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ all_files_list = os.listdir("all_files/")
 
 
 def store_data():
-    dict1 = {}
+    files_data = {}
     for files in all_files_list:
         with open(f"all_files/{files}", "r") as d:
-            dict1[d.name] = d.read().replace("\n", " ")
+            files_data[d.name] = d.read().replace("\n", " ")
             d.close()
-    return dict1
+    return files_data
 
 
 data = store_data()
