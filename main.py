411eae584df2a0c3db1f2dfdf05b29d3f483d747
Aksh <akshdeep434@gmail.com>
2022-04-19 16:09:41 +0530

    Minor code refactoring and case in-sensitive matching

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def store_data():
     files_data = {}
     for files in all_files_list:
         with open(f"all_files/{files}", "r") as d:
-            files_data[d.name] = d.read().replace("\n", " ")
+            files_data[d.name] = d.read().replace("\n", " ").lower()
             d.close()
     return files_data
 
@@ -16,20 +16,15 @@ data = store_data()
 
 def search(txt):
     for key, value in data.items():
-        if txt in value:
-            n = value.count(txt)
-            print(f"Match Found in {key}, Total Occurences: {n}")
-        else:
-            print(f"Sorry, No Match Found in {key}")
+        print(f"Match Found in {key}, Total Occurrences: {value.count(txt)}")\
+            if txt in value else print(f"Sorry, No Match Found in {key}")
 
 
 while True:
-    s = input("Enter text to search: ")
-
+    s = input("Enter text to search: ").lower()
     if s == "quit":
         break
-    else:
-        search(s)
+    search(s)
 
 
 
