5fac7db908a93bb594a09bbd8b239fa33032a4e0
Aksh <akshdeep434@gmail.com>
2022-04-19 15:27:07 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import os
 all_files_list = os.listdir("all_files/")
 
-s = input("Enter text to search: ")
+
+def store_data():
+    dict1 = {}
+    for files in all_files_list:
+        with open(f"all_files/{files}", "r") as d:
+            dict1[d.name] = d.read().replace("\n", " ")
+
+    return dict1
+
+
+data = store_data()
 
 
 def search(txt):
-    for files in all_files_list:
-        n = 0
-        with open(f"all_files/{files}", "r") as data:
-            data_list = data.readlines()
-            for line in data_list:
-                if txt in line:
-                    n += 1
-        print(f"In file {data.name} total no of matches for searched string: {n}")
+    for key, value in data.items():
+        if txt in value:
+            n = value.count(txt)
+            print(f"Match Found in {key}, Total Occurences: {n}")
+        else:
+            print(f"Sorry, No Match Found in {key}")
 
 
-search(s)
+while True:
+    s = input("Enter text to search: ")
+
+    if s == "quit":
+        break
+    else:
+        search(s)
+
+
+
+
 
 
